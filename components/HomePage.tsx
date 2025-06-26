73314c08a6ba4a5943540e20a206664f62dc9460
Sidharath Sidharath <ss.sidharath@gmail.com>
2025-06-26 16:14:24 -0700

    update video again

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
           playsInline
           className="absolute inset-0 h-full w-full object-cover"
         >
-          <source src="/videos/hero3.mp4" type="video/mp4" />
+          <source src="/videos/hero.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
